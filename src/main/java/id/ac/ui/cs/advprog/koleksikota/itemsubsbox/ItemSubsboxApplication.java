96756c148139cabd4e9568fc2d9f4bf192df6371
Kristoforus Adi Himawan <kristoforus.adi28@gmail.com>
2024-04-17 01:15:50 +0700

    Initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/koleksikota/itemsubsbox/ItemSubsboxApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.koleksikota.itemsubsbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemSubsboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemSubsboxApplication.class, args);
+	}
+
+}
