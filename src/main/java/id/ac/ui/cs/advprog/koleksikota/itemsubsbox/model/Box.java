37f019ef4e1ca8bcf9fc3055e623a4ace6b031ba
Kristoforus Adi Himawan <kristoforus.adi28@gmail.com>
2024-04-24 01:33:14 +0700

    [RED] Add abstract class Box

---
+++ src/main/java/id/ac/ui/cs/advprog/koleksikota/itemsubsbox/model/Box.java
@@ -0,0 +1,35 @@
+package id.ac.ui.cs.advprog.koleksikota.itemsubsbox.model;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public abstract class Box implements Component {
+    protected String boxId;
+    @Getter(AccessLevel.NONE)
+    protected String name;
+    protected String description;
+    @Setter(AccessLevel.NONE)
+    protected URL picture;
+    @Setter(AccessLevel.NONE)
+    protected ArrayList<Item> items;
+    @Getter(AccessLevel.NONE)
+    protected Integer price;
+
+    public abstract String getName();
+
+    @SuppressWarnings("deprecation")
+    public void setPicture(String url) throws MalformedURLException {
+        this.picture = new URL(url);
+    }
+
+    public abstract void setItems(ArrayList<Item> items);
+
+    public abstract Integer calculatePrice();
+}
