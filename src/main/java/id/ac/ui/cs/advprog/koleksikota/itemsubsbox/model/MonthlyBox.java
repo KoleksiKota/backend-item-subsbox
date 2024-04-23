13678f7839f69c802e8c6e1054098142fdaa527b
Kristoforus Adi Himawan <kristoforus.adi28@gmail.com>
2024-04-24 02:31:17 +0700

    [GREEN] Implement MonthlyBox model

---
+++ src/main/java/id/ac/ui/cs/advprog/koleksikota/itemsubsbox/model/MonthlyBox.java
@@ -0,0 +1,31 @@
+package id.ac.ui.cs.advprog.koleksikota.itemsubsbox.model;
+
+import java.util.ArrayList;
+
+public class MonthlyBox extends Box {
+    public MonthlyBox() {
+        super();
+    }
+
+    public String getName() {
+        return "MTH - " + name;
+    }
+
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
+        this.calculatePrice();
+    }
+
+    // Untuk mendapatkan untung, harga akan dinaikkan sebesar 5%
+    public Integer calculatePrice() {
+        Integer totalPrice = 0;
+        for (Item item : items) {
+            totalPrice += item.calculatePrice();
+        }
+
+        double finalPrice = totalPrice * 1.05;
+        int roundedFinalPrice = (int) Math.floor(finalPrice);
+        this.setPrice(roundedFinalPrice);
+        return roundedFinalPrice;
+    }
+}
