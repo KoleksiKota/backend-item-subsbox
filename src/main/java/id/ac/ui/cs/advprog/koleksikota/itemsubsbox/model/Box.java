0f1441a119665397a92848e97df168431153e7bf
Kristoforus Adi Himawan <kristoforus.adi28@gmail.com>
2024-05-15 08:13:58 +0700

    [REFACTOR] Set up Item and Box model classes for database integration

--- src/main/java/id/ac/ui/cs/advprog/koleksikota/itemsubsbox/model/Box.java
+++ src/main/java/id/ac/ui/cs/advprog/koleksikota/itemsubsbox/model/Box.java
@@ -2,30 +2,46 @@ package id.ac.ui.cs.advprog.koleksikota.itemsubsbox.model;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Box implements Component {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String name;
     private String description;
     @Setter(AccessLevel.NONE)
     private URL picture;
-    private ArrayList<Item> items;
     @Getter(AccessLevel.NONE)
     @Setter(AccessLevel.NONE)
     private Integer price;
+
+    @ManyToMany
+    @JoinTable(name = "box_item", joinColumns = @JoinColumn(name = "box_id"), inverseJoinColumns = @JoinColumn(name = "item_id"))
+    @Setter(AccessLevel.NONE)
+    private List<Item> items;
 
     @SuppressWarnings("deprecation")
     public void setPicture(String url) throws MalformedURLException {
         this.picture = new URL(url);
     }
 
+    public void setItems(List<Item> items) {
+        this.items = items;
+        this.calculatePrice();
+    }
+
+    @JsonProperty("price")
     public Integer calculatePrice() {
         Integer totalPrice = 0;
         for (Item item : items) {
--- src/main/java/id/ac/ui/cs/advprog/koleksikota/itemsubsbox/model/Item.java
+++ src/main/java/id/ac/ui/cs/advprog/koleksikota/itemsubsbox/model/Item.java
@@ -2,14 +2,22 @@ package id.ac.ui.cs.advprog.koleksikota.itemsubsbox.model;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Item implements Component {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String name;
     private String description;
@@ -18,11 +26,16 @@ public class Item implements Component {
     @Getter(AccessLevel.NONE)
     private Integer price;
 
+    @JsonIgnore
+    @ManyToMany(mappedBy = "items")
+    private List<Box> boxes;
+
     @SuppressWarnings("deprecation")
     public void setPicture(String url) throws MalformedURLException {
         this.picture = new URL(url);
     }
 
+    @JsonProperty("price")
     public Integer calculatePrice() {
         return this.price;
     }
